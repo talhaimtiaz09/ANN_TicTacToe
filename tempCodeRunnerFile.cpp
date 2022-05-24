8677cb82242883b8287471af4f02d05e3bfd9188
Talha <talhaimtiaz555@gmail.com>
2022-05-24 15:27:34 +0500

    Intial commit

--- tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-if(board.evaluate()==1.0){
-		// 	std::cout<<"X won the game\n";
-		// 	break;
-		// }
-		// else if(board.evaluate()==-1.0){
-		// 	std::cout<<"O won the game\n";
-		// 	break;
-		// }
